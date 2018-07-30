124a2fe9698862224552038e6c9bb4f5c6c7600a
sunb001 <2267660581@qq.com>
2018-07-30 13:48:51 +0800

    'update'

--- publish.js
+++ publish.js
@@ -4,6 +4,8 @@
 const shell = require('shelljs');
 const path = require('path');
 const fs = require('fs');
+const isString = require('lodash/isString');
+const isFunction = require('lodash/isFunction');
 
 const root = path.join(__dirname, './');
 
@@ -24,37 +26,65 @@ shell.cd(root);
 // .npmrc添加//registry.npmjs.org/:_authToken=${NPM_TOKEN}
 fs.writeFileSync('.npmrc', `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}`);
 
-// npm version patch
-console.log('npm version patch')
-shell.exec('npm version patch');
-if (shell.error()) {
-    process.exit(1)
+const exec = (command, maybeDescription, maybeErrFn) => {
+    let [description, errFn] = [command, () => {process.exit(1)}];
+
+    if (isString(maybeDescription)) description = maybeDescription;
+    if (isFunction(maybeDescription)) errFn = maybeDescription;
+    if (isFunction(maybeErrFn)) errFn = maybeErrFn;
+
+    console.log(description)
+    shell.exec(command);
+    const err = shell.error()
+    if (err) errFn(err);
 }
+
+// npm version patch
+let isNpmrcCommit = false;
+exec('npm version patch', err => {
+    if (/Git working directory not clean/.test(err)) {
+        // commit .npmrc
+        exec('git add .');
+        exec(`git commit -m 'update' `);
+        exec('npm version patch');
+        isNpmrcCommit = true;
+    }
+})
 
 // npm publish
-console.log('npm publish')
-shell.exec('npm publish');
-if (shell.error()) {
-    process.exit(1)
-}
+// console.log('npm publish')
+// shell.exec('npm publish');
+// if (shell.error()) {
+//     process.exit(1)
+// }
+
+exec('npm publish ')
 
 // 恢复.npmrc文件
-console.log('恢复.npmrc文件')
-shell.exec('git checkout .npmrc');
-if (shell.error()) {
-    process.exit(1)
-}
+// console.log('恢复.npmrc文件')
+// shell.exec('git checkout .npmrc');
+// if (shell.error()) {
+//     process.exit(1)
+// }
+if (isNpmrcCommit) {
+    exec('git reset HEAD^ --hard');    
+} 
+exec('git checkout .npmrc', '恢复.npmrc文件')
+
 
 //  git push && git push --tags
-console.log('git push && git push --tags')
-shell.exec('git push && git push --tags');
-if (shell.error()) {
-    process.exit(1)
-}
+// console.log('git push && git push --tags')
+// shell.exec('git push && git push --tags');
+// if (shell.error()) {
+//     process.exit(1)
+// }
+exec('git push && git push --tags');
+
 
 // cnpm sync @cjfed/cjfec
-console.log('cnpm sync @cjfed/cjfec')
-shell.exec('cnpm sync @cjfed/cjfec');
-if (shell.error()) {
-    process.exit(1)
-}
+// console.log('cnpm sync @cjfed/cjfec')
+// shell.exec('cnpm sync @cjfed/cjfec');
+// if (shell.error()) {
+//     process.exit(1)
+// }
+// exec('cnpm sync @cjfed/cjfec')
